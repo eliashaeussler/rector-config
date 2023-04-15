c5832abc2e8cfc8b58ca18fa9b510a7a2e1d91f9
Elias Häußler <elias@haeussler.dev>
2023-04-15 16:25:57 +0200

    [TASK] Require at least PHPUnit 10.1

--- tests/src/Helper/VersionHelperTest.php
+++ tests/src/Helper/VersionHelperTest.php
@@ -48,7 +48,7 @@ final class VersionHelperTest extends Framework\TestCase
     {
         $actual = Src\Helper\VersionHelper::getPackageVersion('phpunit/phpunit');
 
-        self::assertMatchesRegularExpression('/^10\\.0\\.\\d+$/', $actual);
+        self::assertMatchesRegularExpression('/^10\\.1\\.\\d+$/', $actual);
     }
 
     #[Framework\Attributes\Test]
