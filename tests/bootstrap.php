655989d4e6bb3c40c47fea97e469c0a46ecf7f20
Elias Häußler <elias@haeussler.dev>
2024-01-19 10:50:04 +0100

    [BUGFIX] Avoid colliding class-loading from Rector bootstrap

---
+++ tests/bootstrap.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Composer package "eliashaeussler/rector-config".
+ *
+ * Copyright (C) 2023-2024 Elias Häußler <elias@haeussler.dev>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// Preload ParserFactory from nikic/php-parser to avoid using
+// class-loader from Rector which ships a different version
+// of nikic/php-parser. Can be removed once Rector includes a
+// more recent version of nikic/php-parser.
+$classLoader = require dirname(__DIR__).'/vendor/autoload.php';
+$classLoader->loadClass(\PhpParser\ParserFactory::class);
